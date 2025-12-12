c647599d72443c0646f6c59a6aa0b432ed7786bc
arunabbanapuram23 <arunabbanapuram23@gmail.com>
2025-12-12 10:46:58 +0530

    Add files via upload

---
+++ calculator1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class calculator1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First num: ");
+        int a = sc.nextInt();
+        System.out.println("Second num: ");
+        int b = sc.nextInt();
+        System.out.println("op: ");
+        char c = sc.next().charAt(0);
+        System.out.println(c);
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        float div = (float) a / b;
+
+        if (c == '+') {
+            System.out.println("The sum is equals to " + sum);
+        } else if (c=='-') {
+            System.out.println("The difference is equals to " + diff);
+        } else if (c == '*') {
+            System.out.println("The multiplication is equals to " + mul);
+        } else if (c == '/') {
+            System.out.println("The quotient is equals to " + div);
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+}
+
+   

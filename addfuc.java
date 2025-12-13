44bcb48ca7b0353c5f5c630aa4aca872fc34529f
arunabbanapuram23 <arunabbanapuram23@gmail.com>
2025-12-13 15:55:20 +0530

    Add files via upload

---
+++ addfuc.java
@@ -0,0 +1,13 @@
+public class addfuc
+ {public static void main(String[] args) {
+    System.out.print(add(2,5));
+ }
+ public static int add(int a,int b){
+    return a+b;
+ }
+    
+ }  
+ 
+
+    
+
